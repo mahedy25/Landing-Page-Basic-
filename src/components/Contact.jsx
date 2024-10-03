8a33742228aa9799d8ef67f06d024cd59de6c414
mahedy25 <mahedyhasan873@gmail.com>
2024-10-03 22:25:18 +0600

    some

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section id="contact" className="py-16 bg-blue-400 ">
+    <section id="contact" className="pt-16 bg-blue-400 ">
       <div className="max-w-7xl mx-auto px-6 text-center">
         <h3 className="text-3xl font-semibold">Book an Appointment !</h3>
       </div>
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -29,51 +29,61 @@ const Contactform = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mt-8 max-w-xl mx-auto text-black font-bold space-y-6">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your Name"
-        value={formData.name}
-        onChange={handleChange}
-        className="w-full p-3 border border-gray-300 rounded-md"
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Your Email"
-        value={formData.email}
-        onChange={handleChange}
-        className="w-full p-3 border border-gray-300 rounded-md"
-        required
-      />
-      <input
-        type="date"
-        name="date"
-        value={formData.date}
-        onChange={handleChange}
-        className="w-full p-3 border border-gray-300 rounded-md"
-        required
-      />
-      <textarea
-        name="message"
-        placeholder="Message"
-        value={formData.message}
-        onChange={handleChange}
-        className="w-full p-3 border border-gray-300 rounded-md"
-        required
-      ></textarea>
-      <button
-        type="submit"
-        className="bg-black shadow-md hover:shadow-lg hover:bg-gray-800 active:bg-gray-700 text-white py-3 px-6 rounded-md"
-      >
-        Submit
-      </button>
+      <section id="contact" className="py-16 bg-blue-400 ">
 
-      {/* Toastify Container */}
-    <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick pauseOnFocusLoss draggable pauseOnHover />
-    </form>
+        <div className="max-w-7xl mx-auto px-6 text-center">
+        <form onSubmit={handleSubmit} className="mt-8 max-w-xl mx-auto text-black font-bold space-y-6">
+    <input
+      type="text"
+      name="name"
+      placeholder="Your Name"
+      value={formData.name}
+      onChange={handleChange}
+      className="w-full p-3 border border-gray-300 rounded-md"
+      required
+    />
+    <input
+      type="email"
+      name="email"
+      placeholder="Your Email"
+      value={formData.email}
+      onChange={handleChange}
+      className="w-full p-3 border border-gray-300 rounded-md"
+      required
+    />
+    <input
+      type="date"
+      name="date"
+      value={formData.date}
+      onChange={handleChange}
+      className="w-full p-3 border border-gray-300 rounded-md"
+      required
+    />
+    <textarea
+      name="message"
+      placeholder="Message"
+      value={formData.message}
+      onChange={handleChange}
+      className="w-full p-3 border border-gray-300 rounded-md"
+      required
+    ></textarea>
+    <button
+      type="submit"
+      className="bg-black shadow-md hover:shadow-lg hover:bg-gray-800 active:bg-gray-700 text-white py-3 px-6 rounded-md"
+    >
+      Submit
+    </button>
+
+    {/* Toastify Container */}
+  <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick pauseOnFocusLoss draggable pauseOnHover />
+  </form>
+        </div>
+         
+      </section>
+
+     
+
+  
   );
 };
 
