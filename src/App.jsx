7b1018bd2cd3764e77e7764439d9155d9ee86a4b
mahedy25 <mahedyhasan873@gmail.com>
2024-10-03 22:05:58 +0600

    same

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import About from './components/About'
 import Services from './components/Services'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
-import Footer from './components/Footer'
 import Contactform from './components/contactform'
+import Footer from './components/Footer'
 
 
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,6 @@ const Contact = () => {
     <section id="contact" className="py-16 bg-blue-400 ">
       <div className="max-w-7xl mx-auto px-6 text-center">
         <h3 className="text-3xl font-semibold">Book an Appointment !</h3>
-        
       </div>
     </section>
   );
